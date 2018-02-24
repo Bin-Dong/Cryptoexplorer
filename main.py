df6312988bf496687ee9638b29592339b220df87
ziz19 <ziz19@pitt.edu>
2018-02-24 16:31:46 -0500

    renaming

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import urllib.request
-from bs4 import BeautifulSoup
+from  get_introduction import *
+from  get_api import *
 
-
-name = input("What's the name?")
-link = 'https://info.binance.com/en/currencies/'+name
-request = urllib.request.Request(link)
-response = urllib.request.urlopen(request)
-soup = BeautifulSoup(response,'html.parser')
-text = soup.get_text()
-start = text.find('Introduction')   # start location for introduction
-end = text.find('\n\n\n',start)
-Introduction = text[start:end]
+main()
