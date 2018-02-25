da012299e2ed0a623a74cbd4eb09342812110036
ziz19 <ziz19@pitt.edu>
2018-02-24 21:52:15 -0500

    handle edge cases

--- get_introduction.py
+++ get_introduction.py
@@ -3,9 +3,14 @@ from bs4 import BeautifulSoup
 
 def get_introduction(name):
     '''Accept the name of the currency, return its introduction'''
+    if not name:
+        return
     link = 'https://info.binance.com/en/currencies/'+name
     request = urllib.request.Request(link)
-    response = urllib.request.urlopen(request)
+    try:
+        response = urllib.request.urlopen(request)
+    except urllib.error.HTTPError:
+        return
     soup = BeautifulSoup(response,'html.parser')
     text = soup.get_text()
     start = text.find('Introduction')   # start location for introduction
