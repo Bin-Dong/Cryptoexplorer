1c80bf9d24238ec8af447da255e2a3b4a06b277c
ziz19 <ziz19@pitt.edu>
2018-02-25 10:01:11 -0500

    support machine learning

--- machine_learning_classfication.py
+++ machine_learning_classfication.py
@@ -100,18 +100,5 @@ class MLC:
 
 
 
-f = open('text.txt','r')
-text = f.readlines()
-f.close()
-messages = []
-scores = []
-for l in text:
-    message,score = l.split('^')
-    messages.append(message)
-    scores.append(int(score[:-1]))
-train = messages[:112]
-test = messages[112:]
-m = MLC(train,scores[:112])
-reviews = set(m.get_reviews(test))
-for l in reviews:
-    print(l)
+
+
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from  get_introduction import *
 from  get_api import *
 from twitter_search import *
+from machine_learning_classfication import *
 
 currency = get_coin_info()
 get_introduction(currency)
-messages = get_Twitter_result(currency)
-
-
+# messages = get_Twitter_result(currency)
+f = open('text.txt','r')
+text = f.readlines()
+f.close()
+messages = []
+scores = []
+for l in text:
+    message,score = l.split('^')
+    messages.append(message)
+    scores.append(int(score[:-1]))
+train = messages[:112]
+test = messages[112:]
+m = MLC(train,scores[:112])
+reviews = set(m.get_reviews(test))
+for l in reviews:
+    print(l)
